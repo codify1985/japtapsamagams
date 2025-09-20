9e2808a2d8d101c684d4a0fc6ba5a6a58fcef63a
codify1985 <saluja.atl@gmailom>
2025-09-19 22:40:01 -0500

    reoved album artist columns

--- ui/src/album/AlbumTableView.jsx
+++ ui/src/album/AlbumTableView.jsx
@@ -98,7 +98,7 @@ const AlbumTableView = ({
 
   const toggleableFields = useMemo(() => {
     return {
-      artist: <ArtistLinkField source="albumArtist" />,
+      // artist: <ArtistLinkField source="albumArtist" />,
       songCount: isDesktop && (
         <NumberField source="songCount" sortByOrder={'DESC'} />
       ),
