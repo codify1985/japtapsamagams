8a939dfddaa67474c8e99fac3930919bb08aa199
codify1985 <saluja.atl@gmailom>
2025-09-19 21:28:24 -0500

    Intial Check with some label changes and added Year filter for Samagams

--- ui/src/common/Title.jsx
+++ ui/src/common/Title.jsx
@@ -8,7 +8,7 @@ export const Title = ({ subTitle, args }) => {
   const text = translate(subTitle, { ...args, _: subTitle })
 
   if (isDesktop) {
-    return <span>Navidrome {text ? ` - ${text}` : ''}</span>
+    return <span>Jap Tap Samagams {text ? ` - ${text}` : ''}</span>
   }
   return <span>{text ? text : 'Navidrome'}</span>
 }
